53cdb403169b56c07915864945a070936c67a5b1
Martin Lenweiter <martin@grace-technology.io>
2023-08-11 13:54:31 +0200

    finish log

---
+++ internal/log/log.go
@@ -0,0 +1,213 @@
+package log
+
+import (
+	"errors"
+	log_v1 "github.com/martin-lenweiter/proglog/api/v1"
+	"io"
+	"os"
+	"path"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+type Log struct {
+	activeSegment *segment
+	segments      []*segment
+	Config        Config
+	Dir           string
+	mu            sync.RWMutex
+}
+
+func NewLog(dir string, c Config) (*Log, error) {
+	if c.Segment.MaxStoreBytes == 0 {
+		c.Segment.MaxStoreBytes = 1024
+	}
+	if c.Segment.MaxIndexBytes == 0 {
+		c.Segment.MaxIndexBytes = 1024
+	}
+	l := &Log{
+		Dir:    dir,
+		Config: c,
+	}
+	return l, l.setup()
+}
+
+// When a log starts, it’s responsible for setting itself up for the segments that
+// already exist on disk or, if the log is new and has no existing segments, for
+// bootstrapping the initial segment. We fetch the list of the segments on disk,
+// parse and sort the base offsets (because we want our slice of segments to be
+// in order from oldest to newest), and then create the segments with the
+// newSegment() helper method, which creates a segment for the base offset you
+// pass in.
+func (l *Log) setup() error {
+	files, err := os.ReadDir(l.Dir)
+	if err != nil {
+		return err
+	}
+	var baseOffsets []uint64
+	for _, file := range files {
+		offStr := strings.TrimSuffix(
+			file.Name(),
+			path.Ext(file.Name()),
+		)
+		off, _ := strconv.ParseUint(offStr, 10, 0)
+		baseOffsets = append(baseOffsets, off)
+	}
+	sort.Slice(baseOffsets, func(i, j int) bool {
+		return baseOffsets[i] < baseOffsets[j]
+	})
+	for i := 0; i < len(baseOffsets); i++ {
+		if err = l.newSegment(baseOffsets[i]); err != nil {
+			return err
+		}
+		// baseOffset contains dup for index and store so we skip
+		// the dup
+		i++
+	}
+	if l.segments == nil {
+		if err = l.newSegment(
+			l.Config.Segment.InitialOffset,
+		); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (l *Log) newSegment(off uint64) error {
+	s, err := newSegment(l.Dir, off, l.Config)
+	if err != nil {
+		return err
+	}
+	l.segments = append(l.segments, s)
+	l.activeSegment = s
+	return nil
+}
+
+func (l *Log) Append(record *log_v1.Record) (uint64, error) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	off, err := l.activeSegment.Append(record)
+	if err != nil {
+		return 0, err
+	}
+	if l.activeSegment.IsMaxed() {
+		err = l.newSegment(off + 1)
+	}
+	return off, err
+}
+
+func (l *Log) Read(off uint64) (*log_v1.Record, error) {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+
+	// find correct segment, if not return error
+	seg, err := l.findSegment(off)
+
+	if err != nil {
+		return nil, err
+	}
+	return seg.Read(off)
+}
+
+func (l *Log) findSegment(off uint64) (*segment, error) {
+	for n := len(l.segments) - 1; n >= 0; n-- {
+		if l.segments[n].baseOffset <= off {
+			return l.segments[n], nil
+		}
+	}
+	return nil, errors.New("segment not found")
+}
+
+func (l *Log) Close() error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	for _, seg := range l.segments {
+		err := seg.Close()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (l *Log) Remove() error {
+	if err := l.Close(); err != nil {
+		return err
+	}
+	return os.RemoveAll(l.Dir)
+}
+func (l *Log) Reset() error {
+	if err := l.Remove(); err != nil {
+		return err
+	}
+	return l.setup()
+}
+
+func (l *Log) LowestOffset() (uint64, error) {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+	return l.segments[0].baseOffset, nil
+}
+func (l *Log) HighestOffset() (uint64, error) {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+	off := l.segments[len(l.segments)-1].nextOffset
+	if off == 0 {
+		return 0, nil
+	}
+	return off - 1, nil
+}
+
+// Truncate removes all segments whose highest offset is lower than
+// lowest. Because we don’t have disks with infinite space, we’ll periodically call
+// Truncate() to remove old segments whose data we (hopefully) have processed
+// by then and don’t need anymore.
+func (l *Log) Truncate(lowest uint64) error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	for idx, seg := range l.segments {
+		if seg.nextOffset-1 <= lowest {
+			err := seg.Remove()
+			if err != nil {
+				return err
+			}
+			l.segments = append(l.segments[:idx], l.segments[idx+1:]...)
+		}
+	}
+	return nil
+}
+
+// Reader returns an io.Reader to read the whole log. We’ll need this capability
+// when we implement coordinate consensus and need to support snapshots
+// and restoring a log. Reader() uses an io.MultiReader()
+// call to concatenate the  segments’ stores.
+// The segment stores are wrapped  by the originReader type for two
+// reasons. The first reason is to satisfy the io.
+// Reader interface so we  can pass it
+// into the io.MultiReader() call.
+// The second is to ensure that  we begin reading from
+// the origin of the store and read its entire file.
+func (l *Log) Reader() io.Reader {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+	readers := make([]io.Reader, len(l.segments))
+	for i, segment := range l.segments {
+		readers[i] = &originReader{segment.store, 0}
+	}
+	return io.MultiReader(readers...)
+}
+
+type originReader struct {
+	*store
+	off int64
+}
+
+func (o *originReader) Read(p []byte) (int, error) {
+	n, err := o.ReadAt(p, o.off)
+	o.off += int64(n)
+	return n, err
+}
---
+++ internal/log/log_test.go
@@ -0,0 +1,106 @@
+package log
+
+import (
+	log_v1 "github.com/martin-lenweiter/proglog/api/v1"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
+	"io"
+	"os"
+	"strconv"
+	"testing"
+)
+
+func TestLog(t *testing.T) {
+	for scenario, fn := range map[string]func(
+		t *testing.T, log *Log,
+	){
+		"append and read a record succeeds": testAppendRead,
+		"offset out of range error":         testOutOfRangeErr,
+		"init with existing segments":       testInitExisting,
+		"reader":                            testReader,
+		"truncate":                          testTruncate,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			dir, err := os.MkdirTemp("", "store-test")
+			require.NoError(t, err)
+			defer os.RemoveAll(dir)
+			c := Config{}
+			c.Segment.MaxStoreBytes = 32
+			log, err := NewLog(dir, c)
+			require.NoError(t, err)
+			fn(t, log)
+		})
+	}
+}
+
+func testAppendRead(t *testing.T, log *Log) {
+	appended := &log_v1.Record{Value: []byte("hello world")}
+	off, err := log.Append(appended)
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+	read, err := log.Read(off)
+	require.NoError(t, err)
+	require.Equal(t, read.Value, appended.Value)
+}
+
+func testOutOfRangeErr(t *testing.T, log *Log) {
+	read, err := log.Read(1)
+	require.Nil(t, read)
+	require.Error(t, err)
+}
+
+func testInitExisting(t *testing.T, log *Log) {
+	var appended []*log_v1.Record
+	for i := 0; i < 3; i++ {
+		appended = append(appended, &log_v1.Record{Value: []byte(
+			"hello world" + strconv.Itoa(i))})
+	}
+	for i := 0; i < 3; i++ {
+		_, err := log.Append(appended[i])
+		require.NoError(t, err)
+	}
+	err := log.Close()
+	require.NoError(t, err)
+	log, err = NewLog(log.Dir, log.Config)
+	require.NoError(t, err)
+	high, err := log.HighestOffset()
+	require.NoError(t, err)
+	require.Equal(t, uint64(2), high)
+	low, err := log.LowestOffset()
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), low)
+	for i := 0; i < 3; i++ {
+		rec, err := log.Read(uint64(i))
+		require.NoError(t, err)
+		require.Equal(t, rec.Value, appended[i].Value)
+	}
+
+}
+func testReader(t *testing.T, log *Log) {
+	appended := &log_v1.Record{
+		Value: []byte("hello world"),
+	}
+	off, err := log.Append(appended)
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+	reader := log.Reader()
+	b, err := io.ReadAll(reader)
+	require.NoError(t, err)
+	read := &log_v1.Record{}
+	err = proto.Unmarshal(b[lenWidth:], read)
+	require.NoError(t, err)
+	require.Equal(t, appended.Value, read.Value)
+}
+func testTruncate(t *testing.T, log *Log) {
+	append := &log_v1.Record{
+		Value: []byte("hello world"),
+	}
+	for i := 0; i < 3; i++ {
+		_, err := log.Append(append)
+		require.NoError(t, err)
+	}
+	err := log.Truncate(1)
+	require.NoError(t, err)
+	_, err = log.Read(0)
+	require.Error(t, err)
+}
